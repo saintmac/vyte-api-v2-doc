f477b4589206270cc73e01971316bbfb99f74159
Ladislas Dellinger <ladislas14@gmail.com>
2020-08-24 14:09:44 +0200

    design: fix margin-right for left panel on mobile

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,10 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    [
+      "meta",
+      { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+    ],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
       "meta",
