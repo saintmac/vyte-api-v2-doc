40369e67fc4f4c366a50cd98455b5fd55baa4688
Ladislas Dellinger <ladislas14@gmail.com>
2020-08-27 17:46:57 +0200

    fix color change on hover for endpoints

--- docs/.vuepress/components/Endpoint.vue
+++ docs/.vuepress/components/Endpoint.vue
@@ -4,66 +4,68 @@
       <span v-if="method === 'get'" class="attribute-method-get">GET</span>
       <span v-if="method === 'post'" class="attribute-method-post">POST</span>
       <span v-if="method === 'put'" class="attribute-method-put">PUT</span>
-      <span v-if="method === 'delete'" class="attribute-method-delete">DELETE</span> <span class="attribute-method-path">{{path}}</span>
+      <span v-if="method === 'delete'" class="attribute-method-delete"
+        >DELETE</span
+      >
+      <span class="attribute-method-path">{{ path }}</span>
     </a>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['method', 'path', 'href']
-  }
+export default {
+  props: ["method", "path", "href"],
+};
 </script>
 
 <style lang="scss" scoped>
-.endpoint{
+.endpoint {
   font-size: 13px;
   font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
   margin: 10px 0;
 
-  a.attribute-method{
-    &:hover{
+  a.attribute-method {
+    &:hover {
       text-decoration: none;
 
-      .attribute-method-get{
-        color: #2b71a0;
+      .attribute-method-get {
+        filter: brightness(75%);
       }
 
-      .attribute-method-post{
-        color: #064d37;
+      .attribute-method-post {
+        filter: brightness(75%);
       }
 
-      .attribute-method-put{
-        color: #dac50e;
+      .attribute-method-put {
+        filter: brightness(75%);
       }
 
-      .attribute-method-delete{
-        color: #e4592f;
+      .attribute-method-delete {
+        filter: brightness(75%);
       }
 
-      .attribute-method-path{
-        color: #373c46;
+      .attribute-method-path {
+        filter: brightness(75%);
       }
     }
 
-    .attribute-method-get{
+    .attribute-method-get {
       color: #3a97d4;
     }
 
-    .attribute-method-post{
-      color: #09825d;
+    .attribute-method-post {
+      color: #1f9933;
     }
 
-    .attribute-method-put{
-        color: #e4d448;
-      }
+    .attribute-method-put {
+      color: #e4d448;
+    }
 
-
-    .attribute-method-delete{
+    .attribute-method-delete {
       color: #e56f4a;
     }
 
-    .attribute-method-path{
+    .attribute-method-path {
       color: #697386;
     }
   }
